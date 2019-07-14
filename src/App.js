36b8f12573d49635bdea4da7fe4f0b2563095ab6
Adya Indera <adyaindera@berkeley.edu>
2019-07-14 02:19:09 -0700

    Add router in react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import AssignmentList from './Components/AssignmentList';
+import AddAssignment from './Components/AddAssignment';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
+  const assignmentsExample = [
+    {
+      name: 'Concurrent Cached Fileserver',
+      description: 'Concurrent project description',
+      course: 'CS 61C',
+      deadline: '10/11/2019'
+    },
+    {
+      name: 'B+ Tree Indexing',
+      description: 'B+ Tree database project description',
+      course: 'CS 186',
+      deadline: '10/13/2019'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1>Adya's Assignments Fall 2019</h1>
+        </div>
+      </div>
+
+      <div className="container">
+        <Link to="/add">
+          <button type="button" className="btn btn-success mb-3">
+            <span className="font-weight-bold">+</span> New Assignment
+          </button>
+        </Link>
+        <Route 
+          exact path="/" 
+          render={ () => <AssignmentList assignments={assignmentsExample} /> } 
+        />
+        <Route path="/add" component={AddAssignment} />
+      </div>
+    </Router>
   );
 }
 
